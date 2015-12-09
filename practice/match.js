e85889174d744067d3b1ac4edb9b7b97e400061b
Hua Zhang <billyzhang2010@gmail.com>
2015-12-09 10:50:12 -0800

    handle error, match

---
+++ practice/match.js
@@ -0,0 +1,15 @@
+var match = function match(foo,bar,next) {
+    if ( foo !== bar ) {
+        next( new Error("no match") );
+    } else {
+        var result = true;
+        next(null, result);
+    }
+};
+match(2, 2, function handleMatch(err,result){
+  if ( err ) {
+      console.log('Error: ', err);
+  } else {
+      console.log('Result: ', result);
+  }
+});
