a2a327290df5fd732899329f3976b9527ad9d4fa
Hua Zhang <hua.zhang@wsu.edu>
2016-03-12 12:14:30 -0800

    fs.readFileSync(filename, 'utf-8')

--- nodeschool/learnyounode/myFirstIO.js
+++ nodeschool/learnyounode/myFirstIO.js
@@ -3,3 +3,8 @@ var fs = require('fs');
 var buf = fs.readFileSync(process.argv[2]);
 var lines = buf.toString().split('\n').length - 1;
 console.log(lines);
+
+    // note you can avoid the .toString() by passing 'utf8' as the
+     // second argument to readFileSync, then you'll get a String!
+     //
+     // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
