7b6114a9767e7bb9742ef6c46c1f7a6eeb18960e
Hua Zhang <hua.zhang@wsu.edu>
2016-03-12 20:34:38 -0800

    make it modular

---
+++ nodeschool/learnyounode/myModule.js
@@ -0,0 +1,17 @@
+// Modular 
+// prints a list of files in a given directory.
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function (dir, extension, callback) {
+	fs.readdir(dir,  function (err, list) {
+		if (err) 
+			return callback(err) // early return
+		
+		list = list.filter(function (file) {
+			return path.extname(file) === '.' + extension
+			})	
+		callback(null, list)
+	})
+}
