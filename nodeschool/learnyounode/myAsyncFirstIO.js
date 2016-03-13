11e531fbd632f03c08d8d61627ace90c92b458ce
Hua Zhang <hua.zhang@wsu.edu>
2016-03-12 16:45:31 -0800

    combine callback function into parent function

--- nodeschool/learnyounode/myAsyncFirstIO.js
+++ nodeschool/learnyounode/myAsyncFirstIO.js
@@ -3,12 +3,22 @@ var fs = require('fs');
 
 var  file = process.argv[2];
 
-fs.readFile(file, callback);
 
-function callback(err, data) {
+// fs.readFile(file, callback);
+
+// function callback(err, data) {
+// 	if (err) throw err;
+
+// 	var lines = data.toString().split('\n').length - 1
+
+// 	console.log(lines);
+// }
+
+
+
+fs.readFile(file, function (err, content) {
+
 	if (err) throw err;
-
-	var lines = data.toString().split('\n').length - 1
-
+	var lines = content.toString().split('\n').length - 1;
 	console.log(lines);
-}
+})
