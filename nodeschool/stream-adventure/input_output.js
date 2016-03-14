a7daadfa5bb2ca05f1bdf6fcc5d3da07e9858c45
Hua Zhang <hua.zhang@wsu.edu>
2016-03-14 10:21:28 -0700

    input output

---
+++ nodeschool/stream-adventure/input_output.js
@@ -0,0 +1,12 @@
+
+/*	Name: input_output.js
+*	Date: March 14, 2016
+* Take data from `process.stdin` and pipe it to `process.stdout`.
+* With `.pipe()`. `process.stdin.pipe()` to be exact.
+* Don't overthink this.
+*/
+
+process.stdin.pipe(process.stdout);
+
+
+
