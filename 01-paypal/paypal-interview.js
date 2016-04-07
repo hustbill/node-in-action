e72ebb6a4479c0d968b4e3b6092974e1a26ea4e1
Hua Zhang <hua.zhang@wsu.edu>
2016-04-07 00:20:44 -0700

    Paypal javascript interview questions

---
+++ 01-paypal/paypal-interview.js
@@ -0,0 +1,34 @@
+/*
+ Mar 30, 2015
+Accepted Offer
+Positive Experience
+Average Interview
+Application
+I applied through college or university. The process took 2+ months. I interviewed at PayPal in January 2015.
+Interview
+I interviewed on many different teams and the interview style differed from team to team. The interview process was pretty standard. Ranged from your typical algorithm questions to javascript stuff since they are now heavily invested in JS/Node. It really depends on the team and the job.
+
+Interview Questions
+Explain this:
+var Bob = {
+     name: "Bob",
+     name_fn: function(){
+          return this.name;
+     }
+}
+
+console.log(Bob.name_fn()) //"Bob"
+var fn = Bob.name_fn
+console.log(fn()) //undefined  
+*/
+
+var Bob = {
+     name: "Bob",
+     name_fn: function(){
+          return this.name;
+     }
+}
+
+console.log(Bob.name_fn()) //"Bob"
+var fn = Bob.name_fn
+console.log(fn()) //undefined  
