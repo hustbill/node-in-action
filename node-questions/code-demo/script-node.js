e7a4cf11a8dc5bb4d964c4166d4cc37855f06371
hustbill <billyzhang2010@gmail.com>
2020-01-14 14:06:01 -0800

    script-node.js

--- node-questions/code-demo/script-node.js
+++ node-questions/code-demo/script-node.js
@@ -1,7 +1,35 @@
 // 2020-1-14 React Adanced Topics, (NodeJS part)
-console.log('past')
+const fs = require('fs');
+const {spawn} = require('child_process')
+const spawnedProcess = spawn("node spawn.js");
 
-setTimeout(() => {
-    console.log('future')
-}, 1000)
-console.log('current')
+
+// pm2 monitor the child process
+
+
+// console.log('past')
+// fs.readFile('./oneline.txt', 'utf8', (error, data) => {
+//     console.log(data);
+// });
+
+// // fs.readFile('./script-node.js', 'utf8', (error, data) => {
+// //     console.log(data);
+// // });
+
+// process.nextTick(() => {
+//     console.log('next tick code');
+// })
+// setTimeout(() => {
+//     console.log('future')
+//     console.log('set timeout')
+// }, 1000)
+// console.log('present')
+
+/*
+ fs 读写有时候比 current 慢， 
+past
+current
+hell world
+future
+
+*/
